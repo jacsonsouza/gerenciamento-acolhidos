14b96ab9300d860d218ab631efdebfae275246ac
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-04-03 18:49:04 -0300

    Created components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
