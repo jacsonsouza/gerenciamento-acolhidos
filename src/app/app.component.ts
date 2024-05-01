a69968e31a5ea793ff397b45ce4cdfc2ea2cefcb
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-05-01 18:31:39 -0300

    Added components and configured routes to navegation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, ContatosComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'agendamento-acolhidos';
+  title = 'gerenciamento-acolhidos';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contatos', component: ContatosComponent },
+];
