9b4fa088d8868da4f7b475b1d3a8e90ae53545af
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-05-15 19:48:18 -0300

    Added home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 import { FooterComponent } from './components/footer/footer.component';
-
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent,FooterComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gerenciamento-acolhidos';
--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TexthomeComponent } from './texthome.component';
+import { HomeComponent } from './home.component';
 
-describe('TexthomeComponent', () => {
-  let component: TexthomeComponent;
-  let fixture: ComponentFixture<TexthomeComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TexthomeComponent]
+      imports: [HomeComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(TexthomeComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
