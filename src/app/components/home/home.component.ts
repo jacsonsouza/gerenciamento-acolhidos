8de11a62c48ca98424fa818ed2ee6f5338afb6a6
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-05-15 20:59:39 -0300

    Added interpolation and directive ngFor

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  partners = [
+    {
+      img: 'https://www.intelidata.inf.br/site/wp-content/uploads/2017/10/como-administrar-uma-empresa-desde-o-inicio-1200x640-c-top-center.jpg',
+      name: 'Empresas X',
+    },
+    {
+      img: 'https://guarapuava.pr.gov.br/wp-content/uploads/2020/03/Fachada-Prefeitura-13-scaled.jpg',
+      name: 'Prefeitura de Guarapuava',
+    },
+    {
+      img: 'https://humanidades.com/wp-content/uploads/2023/02/Empresa-Unsplash.jpg',
+      name: 'Empresa Y',
+    },
+  ];
 }
