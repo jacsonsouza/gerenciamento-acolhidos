62fdaaf07c31360f6ed81bd231f76625a1dc0f97
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-05-16 19:20:09 -0300

    Added routes and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'appointments', component: AppointmentsComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'about', component: AboutComponent },
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [[RouterLink, RouterOutlet, RouterLinkActive]],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
